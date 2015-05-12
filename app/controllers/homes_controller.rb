b19b5ff66b3efafe4f09891f99e581fcf64ff6f4
Larry Chartier <larrychartier@gmail.com>
2015-05-12 19:25:07 -0400

    added authenticate_user

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,7 +1,7 @@
 class HomesController < ApplicationController
   before_action :set_home, only: [:show, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update, :destroy]
-  #before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @homes = Home.all
